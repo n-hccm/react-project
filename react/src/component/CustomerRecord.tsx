b1e78cba03e8346ec7a33f4e23dabb71665f063b
wasadmin <wasadmin@example.com>
2025-09-19 05:27:25 -0400

    CustomerRecord Static

--- react/src/component/CustomerRecord.tsx
+++ react/src/component/CustomerRecord.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CustomerRecord: React.FC = () => {
-	return (
-		<div>
-			<h2>Customer Record</h2>
-			<p>This is a simple customer record component.</p>
-		</div>
-	);
+    const initialFormObject: any = { name: 'name1', email: 'email1', password: 'password1' };
+    const [formObject, setFormObject] = useState(initialFormObject);
+
+    const changeHandler = function (event: any) {
+        const name = event.target.name;
+        const value = event.target.value;
+        formObject[name] = value;
+        setFormObject({ ...formObject })
+    }
+
+    const deleteSelected = () => {
+        console.log("deleteSelected")
+    }
+
+    const saveSelected = () => {
+        console.log("saveSelected")
+    }
+
+    const cancelSelected = () => {
+        console.log("cancelSelected")
+    }
+
+    return (
+        <div>
+            <h2>Customer Record</h2>
+            <button>Add</button>
+            <button>Update</button>
+            <div>
+                <input type="text" name={"name"} value={formObject.name} placeholder="Name" onChange={(e) => changeHandler(e)} />
+                <input type="email" name={"email"} value={formObject.email} placeholder="Email" onChange={(e) => changeHandler(e)} />
+                <input type="password" name={"password"} value={formObject.password} placeholder="Password" onChange={(e) => changeHandler(e)} />
+                <div>
+                    <input type="button" value="delete" onClick={() => deleteSelected()} />
+                    <input type="button" value="save" onClick={() => saveSelected()}/>
+                    <input type="button" value="cancel" onClick={() => cancelSelected()}/>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CustomerRecord;
