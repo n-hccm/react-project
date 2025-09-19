b8f75a34c7bf37101b4acc7fc5ca78b8a47a9da2
wasadmin <wasadmin@example.com>
2025-09-19 06:21:44 -0400

    Error solutins

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,9 +7,9 @@ import CustomerRecord from './component/CustomerRecord'
 //create a basic example list of customer records.
 //Style: name, email, password
 const staticData = [
-    {name: "John Doe", email: "jdoe@example.com", password: "password123"},
-    {name: "Jane Smith", email: "janesmith@example.com",password: "mypassword"},
-    {name: "Alice Johnson", email: "aj@example.com", password: "alice2024"}
+    {uid: 1, name: "John Doe", email: "jdoe@example.com", password: "password123"},
+    {uid: 2, name: "Jane Smith", email: "janesmith@example.com",password: "mypassword"},
+    {uid: 3, name: "Alice Johnson", email: "aj@example.com", password: "alice2024"}
 ]
 
 function App() {
--- react/src/types/Customer.ts
+++ react/src/types/Customer.ts
@@ -1,4 +1,5 @@
 export type Customer = {
+  uid: number;
   name: string;
   email: string;
   password: string;
